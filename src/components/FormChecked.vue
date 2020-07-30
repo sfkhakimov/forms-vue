c849be17bfb14701b8467c1fcef8385836939e0e
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-07-31 02:09:15 +0300

    Создал компонент чекбокса

---
+++ src/components/FormChecked.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <label>
+      {{ label }}
+      <p v-for="input in initialValue" :key="input.id">
+        {{ input.text }}
+        <input
+          :value="input.value"
+          :type="type"
+          @change="$emit('input', checked)"
+          v-model="checked"
+        />
+      </p>
+    </label>
+    <span>{{ errorText }}</span>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      checked: this.value,
+    };
+  },
+  props: {
+    value: [String, Boolean],
+    label: String,
+    required: Boolean,
+    touched: Boolean,
+    errorText: String,
+    initialValue: Array,
+    name: {
+      type: String,
+      default: "text",
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  padding-bottom: 2px;
+  margin-top: 8px;
+  display: flex;
+  flex-direction: column;
+}
+label {
+  padding-bottom: 2px;
+  margin-top: 8px;
+}
+span {
+  color: red;
+  font-size: 12px;
+}
+</style>>
